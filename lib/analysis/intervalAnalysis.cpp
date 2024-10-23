758bd7393bfcd8b0af098040b0533c9385fd57d5
ling-wyj <47814487+Ling-WYJ@users.noreply.github.com>
2024-10-23 17:01:15 +0800

    brandnew lab1

--- lib/analysis/intervalAnalysis.cpp
+++ lib/analysis/intervalAnalysis.cpp
@@ -1,21 +1,34 @@
 #include "intervalAnalysis.h"
-
-#include <algorithm>
-#include <array>
 #include <queue>
-#include <vector>
 
 using namespace fdlang;
 using namespace fdlang::analysis;
 
-/**
- * Your code starts here
- */
+void IntervalAnalysis::fixedPoint() {
+    // ini States and worklist
+    inputStates[0] = iniStates();
+    worklist.push(0);
 
-void IntervalAnalysis::run() {
+    // worklist Iteration here
+    while (!worklist.empty()) {
+        auto label = worklist.front();
+        worklist.pop();
+        auto inst = insts[label];
+        States &inputState = inputStates[label];
+        States outputState;
+        // transfer Inst
+        outputState = transfer(inst, inputState);
+        // add Successors
+        addSuccessors(label, outputState);
+    }
+}
 
+/****************************************************************
+********************* Your code starts here *********************
+*****************************************************************/
+
+void IntervalAnalysis::checkInstsStates() {
     // You can modify this function arbitrarily
-
     for (auto inst : insts) {
         if (inst->getInstType() != IR::InstType::CheckIntervalInst)
             continue;
@@ -27,4 +40,23 @@ void IntervalAnalysis::run() {
 
         results[checkInst] = ResultType::UNREACHABLE;
     }
+}
+
+States IntervalAnalysis::iniStates() {
+    // You can modify this function arbitrarily
+    return States();
+}
+
+States IntervalAnalysis::transfer(IR::Inst *inst, States &input) {
+    // You can modify this function arbitrarily
+    return States();
+}
+
+bool IntervalAnalysis::joinInto(const States &x, States &y) {
+    // You can modify this function arbitrarily
+    return true;
+}
+
+void IntervalAnalysis::addSuccessors(size_t nowLabel, States outputState) {
+    // You can modify this function arbitrarily
 }
--- lib/analysis/intervalAnalysis.h
+++ lib/analysis/intervalAnalysis.h
@@ -5,16 +5,35 @@
 
 #include <algorithm>
 #include <map>
+#include <queue>
 #include <vector>
 
 namespace fdlang::analysis {
-
+struct Interval {
+    long long l = 0, r = 0;
+    bool isBottom = true;
+    bool operator==(const Interval &o) const {
+        if (isBottom != o.isBottom)
+            return false;
+        if (isBottom == true)
+            return true;
+        return l == o.l && r == o.r;
+    }
+};
+// variable -> value
+using States = std::map<std::string, Interval>;
 class IntervalAnalysis : public DataflowAnalysis {
 public:
     enum class ResultType { YES, NO, UNREACHABLE };
 
 private:
     std::map<IR::CheckIntervalInst *, ResultType> results;
+
+    // label -> states
+    // label is regarded as the index of inst in insts
+    std::map<size_t, States> inputStates;
+
+    std::queue<size_t> worklist;
 
 public:
     IntervalAnalysis(const IR::Insts &insts) : DataflowAnalysis(insts) {}
@@ -42,12 +61,64 @@ public:
         }
     }
 
-    void run() override;
+    void run() override {
 
-private:
+        fixedPoint();
+
+        checkInstsStates();
+    };
+
     /**
-     * Your code starts here
+     * @brief compute Fix Point
+     * 
      */
+    void fixedPoint();
+
+    /****************************************************************
+    ********************* Your code starts here *********************
+    *****************************************************************/
+public:
+    /**
+     * @brief check insts states
+     * 
+     */
+    void checkInstsStates();
+
+    /**
+     * @brief collect all variables and set their value to bottom
+     * 
+     */
+    States iniStates();
+
+    /**
+     * @brief transfer function
+     * 
+     * @param inst 
+     * @param input 
+     * @return States 
+     */
+    States transfer(IR::Inst *inst, States &input);
+
+    /**
+     * @brief check if sucInputStates == sucInputStates ⊔ outputState  or not
+     * @param outputState 
+     * @param sucInputStates 
+     * @return true 
+     * @return false 
+     */
+    bool joinInto(const States &outputState, States &sucInputStates);
+
+    /**
+     * @brief check if now inst outputState change will influence its successors inputState change
+     * and add its influenced successors to worklist
+     * 
+     * @param nowLabel 
+     * @param outputState 
+     * @see inst label:  inst->getLabel()  
+     * inst Successor: inst->getSuccessors()
+     * 
+     */
+    void addSuccessors(size_t nowLabel, States outputState);
 };
 
 } // namespace fdlang::analysis
